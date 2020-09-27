7ac8737d471ef1524bbf35c68e9e35803d2530d3
Shreshth Arora <sarora_be18@thapar.edu>
2020-09-28 03:56:49 +0530

    with exception

--- vacc/settings.py
+++ vacc/settings.py
@@ -101,7 +101,8 @@ try:
     if DATABASE_URL_EXIST:
         DATABASES['default'] = dj_database_url.config(
             conn_max_age=600, ssl_require=True)
-
+except Exception:
+    print('Not in Herko Env')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
